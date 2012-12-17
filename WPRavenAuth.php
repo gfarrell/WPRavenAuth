3db90a772b1758e4251798ab197e30f5fa3a0f82
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-17 23:40:34 +0000

    Created an entry point script

---
+++ WPRavenAuth.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    WPRavenAuth - Raven authentication for Wordpress
+    ================================================
+
+    @license BSD 3-Clause http://opensource.org/licenses/BSD-3-Clause
+    @author  Gideon Farrell <me@gideonfarrell.co.uk>
+    @url     https://github.com/gfarrell/WPRavenAuth
+ */
+
+// Load required files
+require('app/ldap.php');    // LDAP lookups for users
+?>
