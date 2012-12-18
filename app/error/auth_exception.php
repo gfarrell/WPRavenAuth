f6f537c95e373926376e62d0f7d57cc720981384
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-18 00:23:50 +0000

    Created some exception classes

---
+++ app/error/auth_exception.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    AuthException
+    -------------
+
+    Handles authentication errors.
+
+    @file    auth_exception.php
+    @license BSD 3-Clause
+    @package WPRavenAuth
+    @author  Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+namespace WPRavenAuth;
+
+class AuthException extends WPRavenAuth\WPRException {
+    // TODO!
+}
+?>
---
+++ app/error/exception.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    WPRException
+    ------------
+
+    Basic exception class
+
+    @file    exception.php
+    @license BSD 3-Clause
+    @package WPRavenAuth
+    @author  Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+namespace WPRavenAuth;
+
+class WPRException extends Exception {
+
+}
+?>
