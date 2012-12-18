69f06b85cb7b6c9ca088c33587b49626d6f9ea2a
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-18 12:29:00 +0000

    Integrated Config class

--- app/core/ldap.php
+++ app/core/ldap.php
@@ -13,41 +13,23 @@
 namespace WPRavenAuth;
 
 class LDAP {
-    protected static $_config = array(
-        'server' => 'ldap.lookup.cam.ac.uk',
-        'base'   => 'ou=people,o=University of Cambridge,dc=cam,dc=ac,dc=uk',
-        'port'   => '636'
-    );
-
-    public static function config($what = null) {
-        if(is_null($what)) {
-            return LDAP::_config;
-        }
-
-        if(array_key_exists($what, LDAP::_config) {
-            return LDAP::_config[$what];
-        } else {
-            return false;
-        }
-    }
     public static function connect() {
         static $con = null;
 
         if(is_null($con)) {
-            $con = ldap_connect(LDAP::config('server'));
+            $con = ldap_connect(Config::get('ldap.server'));
         }
 
         return $con;
     }
     public static function search($query, $field='givenname') {
         $ds = LDAP::connect();
-        $cfg = LDAP::config();
 
         $result = array();
 
         if ($ds) {
             $resource = ldap_bind($ds);
-            $search   = ldap_search($ds, Ldap::config('base'), "($field=$query)");
+            $search   = ldap_search($ds, Config::get('ldap.base'), "($field=$query)");
             $entries  = ldap_get_entries($ds, $search);
             $prop_map = array(
                 'uid'           =>  'crsid',
