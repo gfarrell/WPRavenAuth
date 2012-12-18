e1c0cf13f526a955e23fa3de9ffb329871eead5b
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-18 00:24:11 +0000

    Added basic config class (TODO!)

---
+++ app/core/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    Config
+    ------
+
+    Basic configuration wrapper class to get/set options from WP.
+
+    @file    config.php
+    @license BSD 3-Clause
+    @package WPRavenAuth
+    @author  Gideon Farrell <me@gideonfarrell.co.uk>
+ */
+
+namespace WPRavenAuth;
+
+class Config {
+    // !TODO
+}
+?>
