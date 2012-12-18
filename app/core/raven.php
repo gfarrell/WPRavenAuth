f30a6197af2ecd8c8a1e53679bce5464534285c3
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-18 00:24:04 +0000

    Added Raven authentication class

---
+++ app/core/raven.php
@@ -0,0 +1,62 @@
+<?php
+/*
+    Raven
+    -----
+
+    Provides wrappers for logging in and out, as well as user validation.
+
+    @file    raven.php
+    @license BSD 3-Clause
+    @package WPRavenAuth
+    @author  Gideon Farrell <me@gideonfarrell.co.uk>  
+ */
+
+namespace WPRavenAuth;
+
+require('ucam_webauth.php');
+
+class Raven {
+    protected static $webauth = null;
+
+    public function login() {
+        if(is_null(Raven::webauth)) {
+            Raven::webauth = new Ucam_Webauth(array(
+                'key_dir'       => WPRavenAuth_keys,
+                'cookie_key'    => Config::get('cookie'),
+                'hostname'      => home_url()
+            ));
+        }
+
+        $auth = Raven::webauth->authenticate();
+
+        if(!$auth) throw new WPRavenAuth\AuthException($webauth->status(), $webauth->msg());
+
+        if(Raven::webauth->success()) {
+            $this->authenticate();
+        }
+    }
+
+    public function logout() {
+        // TODO
+    }
+
+    public function authenticate() {
+        $crsid  = Raven::webauth->principal();
+        
+        if($this->userExists($crsid)) {
+            $user = $this->getWpUser($crsid);
+            $password = $this->_pwd($user->user_pass);
+        }
+
+        // If authorised, continue, otherwise throw them out.
+    }
+
+    public function userExists($crsid) {
+        // TODO
+    }
+
+    public function getWpUser($crsid) {
+        return get_userdatabylogin($crsid);
+    }
+}
+?>
