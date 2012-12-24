5453edfc3a1077561af7cedf54c5f3d2ec141675
Gideon Farrell <me@gideonfarrell.co.uk>
2012-12-24 14:36:48 +0000

    Made raven into a singleton and added doc blocks

--- app/core/raven.php
+++ app/core/raven.php
@@ -16,34 +16,93 @@ namespace WPRavenAuth;
 require('ucam_webauth.php');
 
 class Raven {
+    /**
+     * $salt
+     * Contains the password hashing salt.
+     * 
+     * @static
+     * @var    string
+     * @access public
+     */
     public static $salt = '1wr0auZmxEsdRNVS3GZNX6Qf5XSO7yHZ';
 
-    protected static $webauth = null;
+    /**
+     * $webauth
+     * Contains the ucam_webauth instance.
+     * 
+     * @var    Ucam_Webauth
+     * @access protected
+     */
+    protected $webauth = null;
 
-    public function login() {
-        if(is_null(Raven::webauth)) {
-            Raven::webauth = new Ucam_Webauth(array(
+
+    /**
+     * getInstance
+     * Creates or retrieves the Singleton instance.
+     * 
+     * @access public
+     *
+     * @return Raven instance
+     */
+    public function &getInstance() {
+        static $instance;
+
+        if(is_null($instance)) {
+            $instance = new Raven();
+        }
+
+        return $instance;
+    }
+
+
+    /**
+     * login
+     * Logs the user in.
+     * 
+     * @access public
+     *
+     * @return void
+     */
+    public static function login() {
+        if(is_null($this->webauth)) {
+            $this->webauth = new Ucam_Webauth(array(
                 'key_dir'       => WPRavenAuth_keys,
                 'cookie_key'    => Config::get('cookie'),
                 'hostname'      => home_url()
             ));
         }
 
-        $auth = Raven::webauth->authenticate();
+        $auth = $this->webauth->authenticate();
 
-        if(!$auth) throw new WPRavenAuth\AuthException($webauth->status(), $webauth->msg());
+        if(!$auth) throw new WPRavenAuth\AuthException($this->webauth->status(), $this->webauth->msg());
 
-        if(Raven::webauth->success()) {
+        if($this->webauth->success()) {
             $this->authenticate();
         }
     }
 
+    /**
+     * logout
+     * Logs the user out.
+     * 
+     * @access public
+     *
+     * @return void
+     */
     public function logout() {
         // TODO
     }
 
+    /**
+     * authenticate
+     * Tests if a user is allowed access.
+     * 
+     * @access public
+     *
+     * @return boolean authorised?
+     */
     public function authenticate() {
-        $crsid  = Raven::webauth->principal();
+        $crsid = $this->webauth->principal();
 
         // If authorised, continue, otherwise throw them out.
         $restrictions = Config::get('users.restrictions');
@@ -58,6 +117,17 @@ class Raven {
         return true;
     }
 
+    /**
+     * _testRestriction
+     * Tests a restriction (type, allowed) against a user
+     * 
+     * @param array  $restriction The restriction specification (array[type],[allowed]=>array).
+     * @param WPUser $user        The user object.
+     *
+     * @access protected
+     *
+     * @return boolean test result
+     */
     protected function _testRestriction($restriction, $user) {
         switch($restriction['type']) {
             case 'crsid':
@@ -75,8 +145,30 @@ class Raven {
         // TODO
     }
 
+    /**
+     * getWpUser
+     * Retrieves the WP User object
+     * 
+     * @param string $crsid User's CRSID.
+     *
+     * @access public
+     *
+     * @return WPUser object
+     */
     public function getWpUser($crsid) {
         return get_userdatabylogin($crsid);
+    }
+
+    /**
+     * _pwd
+     * Returns the generic password hash, since passwords aren't important for SSO, but are for WP.
+     * 
+     * @access private
+     *
+     * @return string password
+     */
+    private function _pwd() {
+        return md5(Raven::salt);
     }
 }
 ?>
