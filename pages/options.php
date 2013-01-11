5451b54064f6a311c1453daa00dd9953d12450c6
Gideon Farrell <me@gideonfarrell.co.uk>
2013-01-11 17:03:44 +0000

    Start an options page

---
+++ pages/options.php
@@ -0,0 +1,10 @@
+<div class="wrap">
+<?php screen_icon(); ?>
+<h2>Raven Auth Settings</h2>
+
+<form method="post" action="options.php">
+    <fieldset>
+        <legend>LDAP Settings</legend>
+        
+    </fieldset>
+</form>
