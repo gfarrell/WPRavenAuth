a5a968c67b29f9fb19f12e38fa9dea1495df93ae
Conor Burgess <Burgess.Conor@gmail.com>
2013-10-03 00:23:43 +0100

    Implement per page/post authentication settings using Ibis. Closes #13 :)

--- WPRavenAuth.php
+++ WPRavenAuth.php
@@ -33,7 +33,8 @@ require('app/core/ibis.php');               // Use Ibis database; much more robu
 require('app/lib/ucam_webauth.php');        // Cantab authentication library
 require('app/core/raven.php');              // Interface between WP and Raven
 require('app/error/auth_exception.php');    // Exceptions
-require('pages/options.php');
+require('pages/options.php');               // Options page for wp-admin
+require('app/core/custom_fields.php');      // Custom fields for visibility settings
     
 // Initialise Raven
 add_action('init', 'WPRavenAuth\setup');
@@ -50,11 +51,9 @@ function setup()
     add_action('login_init', 'WPRavenAuth\login_init');                             // Intercept login
     add_action('wp_logout', array(Raven::getInstance(), 'logout'));                 // Intercept logout
     
-    // Add action hooks for authentication on pages
-    
-    
-    // Add custom fields to all pages for visibility
-    
+    // Add filters for authentication on pages
+    add_filter('the_posts', 'WPRavenAuth\showPost');
+    add_filter('get_pages', 'WPRavenAuth\showPost');
 }
     
 // Decide if login should use raven or not, and initiate raven if required
@@ -103,6 +102,80 @@ function  disable_function()
 function check_passwords($username, $password1, $password2)
 {
 	return $password1 = $password2 = Raven::_pwd($username); // This is how Raven does passwords
+}
+    
+/**
+ * Returns the current user.
+ *
+ * @return WP_User
+ */
+function getCurrentUser()
+{
+    if (!function_exists('get_userdata')) {
+        require_once(ABSPATH . WPINC . '/pluggable.php');
+    }
+    
+    //Force user information
+    return wp_get_current_user();
+}
+    
+function userCanAccessPost($postID, $crsid)
+{
+    $postVisibility = get_field('custom_visibility', $postID);
+    
+    if (in_array('public', $postVisibility))
+        return true;
+    elseif (in_array('raven', $postVisibility))
+        return is_user_logged_in();
+    else
+    {
+        $person = Ibis::getPerson($crsid);
+        foreach ($postVisibility as $inst)
+        {
+            $inst_split = explode('-',$inst);
+            if (strcmp($inst_split[0], 'COLL') == 0)
+            {
+                if (Ibis::isMemberOfCollege($person, $inst_split[1]))
+                    return true;
+            }
+            elseif (strcmp($inst_split[0], 'INST') == 0)
+            {
+                if (Ibis::isMemberOfInst($person, $inst_split[1]))
+                    return true;
+            }
+        }
+    }
+    return false;
+}
+    
+function showPost($aPosts = array())
+{
+    $aShowPosts = array();
+    $userCRSID = '';
+    if (is_user_logged_in())
+    {
+        $currentUser = getCurrentUser();
+        $userCRSID = $currentUser->user_login;
+    }
+    foreach ($aPosts as $aPost)
+    {
+        if (!userCanAccessPost($aPost->ID,$userCRSID))
+        {
+            $aPost->post_title = "Restricted Content";
+            $postContent = get_field('error_message', $aPost->ID);
+            if (!is_user_logged_in())
+            {
+                $postContent .= '<p>You may be able to access this content if you <a href="' . wp_login_url() . '">login</a>.</p>';
+            }
+            $aPost->post_content = $postContent;
+            $aPost->post_excerpt = get_field('error_message', $aPost->ID);
+        }
+        $aShowPosts[] = $aPost;
+    }
+    
+    $aPosts = $aShowPosts;
+    
+    return $aPosts;
 }
     
 } // End namespace
